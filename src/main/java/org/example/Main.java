c9a606df9fab9243ca985141da871bb330403b5e
zakh.al.v <Zakharov.A.V2@citilink.ru>
2025-09-03 14:19:35 +0300

    10 first tasks

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,111 @@
+package org.example;
+
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Сумма от 1 до N
+//        int initNumber = scanner.nextInt();
+//        int sum = 0;
+//        for (int i = 1; i <= initNumber; i++) {
+//            sum += i;
+//        }
+//
+//        System.out.println(sum);
+
+        // Сумма квадратов нечётных ≤ N
+//        int initNumber = scanner.nextInt();
+//        double sum = 0;
+//        for (int i = 1; i <= initNumber; i += 2) {
+//            sum += Math.pow(i, 2);
+//        }
+//
+//        System.out.println(sum);
+
+
+        // Факториал N
+//        int factorial = 1;
+//        int initNumber = scanner.nextInt();
+//        for (int i = initNumber; i >= 1; i--) {
+//            factorial *= i;
+//        }
+//
+//        System.out.println(factorial);
+
+        // Среднее арифметическое N чисел
+//        int initNumber = scanner.nextInt();
+//        double average = 0.0;
+//        int sum = 0;
+//        int targetNumber = scanner.nextInt();
+//        int count = 0;
+//        for (int i = initNumber; i <= targetNumber; i++) {
+//            sum += i;
+//            count++;
+//        }
+//
+//        average = (double) sum / (double) count;
+//        System.out.println(average);
+
+        // Подсчёт положительных из N вводимых
+//        int number;
+//        int count = 0;
+//        do {
+//            number = scanner.nextInt();
+//            if (number > 0) {
+//                count++;
+//            }
+//        } while (number != 0);
+//
+//        System.out.println(count);
+
+        // Степени двойки до N
+//        int targetNumber = scanner.nextInt();
+//        for (long i = 1; i <= targetNumber; i *= 2) {
+//            System.out.println(i);
+//        }
+
+        // Сумма цифр числа
+//        int number = scanner.nextInt();
+//        int sum = 0;
+//        for (; number > 0; number /= 10) {
+//            if (number == 0) {
+//                continue;
+//            }
+//
+//            sum += number % 10;
+//        }
+//
+//        System.out.println(sum);
+
+        //Разворот числа
+//        int number = scanner.nextInt();
+//        StringBuilder newNumber = new StringBuilder();
+//        int i = 0;
+//        while (number > 0) {
+//            i = number % 10;
+//            number /= 10;
+//            newNumber.append(i);
+//        }
+//
+//        System.out.println(newNumber);
+
+        // Проверка числа на простоту
+//        int inputNumber = scanner.nextInt();
+//        boolean isSimple = false;
+//        for (int i = 2; i * i <= inputNumber; i++) {
+//            if (inputNumber % i == 0) {
+//                isSimple = true;
+//                break;
+//            }
+//        }
+//
+//        System.out.println(isSimple ? "NO" : "YES");
+
+        // Арифметическая прогрессия
+        
+    }
+}
